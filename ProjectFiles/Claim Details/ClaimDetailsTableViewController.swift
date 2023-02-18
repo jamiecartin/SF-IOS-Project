d097e9dcf5fa837fb829893ffaaf9c7e77da6c66
jaseyaws <jcartin@ykw.systems>
2023-02-18 02:00:14 -0500

    claim details view

---
+++ ProjectFiles/Claim Details/ClaimDetailsTableViewController.swift
@@ -0,0 +1,37 @@
+import UIKit
+import SalesforceSDKCore
+
+class ClaimDetailsTableViewController: UITableViewController {
+
+	var claimId: String?
+	var dataSource: ObjectLayoutDataSource!
+	let reuseIdentifier = "CaseDetailPrototype"
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		if let caseId = claimId {
+			self.dataSource = ObjectLayoutDataSource(objectType: "Case", objectId: caseId, cellReuseIdentifier: self.reuseIdentifier) { field, cell in
+				cell.textLabel?.text = field.value
+				cell.detailTextLabel?.text = field.label
+			}
+			self.dataSource.delegate = self
+			self.tableView.delegate = self
+			self.tableView.activityIndicatorView.startAnimating()
+			self.tableView.dataSource = dataSource
+			self.refreshControl = UIRefreshControl()
+			refreshControl?.addTarget(self.dataSource, action: #selector(self.dataSource.fetchData), for: UIControl.Event.valueChanged)
+			self.tableView.addSubview(refreshControl!)
+			self.dataSource.fetchData()
+		}
+	}
+}
+
+extension ClaimDetailsTableViewController: ObjectLayoutDataSourceDelegate {
+	func objectLayoutDataSourceDidUpdateFields(_ dataSource: ObjectLayoutDataSource) {
+		DispatchQueue.main.async {
+			self.tableView.reloadData()
+			self.refreshControl?.endRefreshing()
+			self.tableView.activityIndicatorView.stopAnimating()
+		}
+	}
+}
