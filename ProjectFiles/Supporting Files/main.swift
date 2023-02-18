6cc16af6687b959e75de556829edd5d8e8a59645
jaseyaws <jcartin@ykw.systems>
2023-02-18 02:19:24 -0500

    main.swift added

---
+++ ProjectFiles/Supporting Files/main.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+import SalesforceSDKCore
+
+UIApplicationMain(
+    CommandLine.argc,
+    CommandLine.unsafeArgv,
+    NSStringFromClass(SFApplication.self),
+    NSStringFromClass(AppDelegate.self)
+)
